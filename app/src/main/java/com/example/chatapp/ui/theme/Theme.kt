df7fb14f2ed9b0ac0ac7b5c6999eb1f9e4e0fabf
programmer <softwaree40@gmail.com>
2022-05-27 04:25:19 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/chatapp/ui/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.example.chatapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun ChatAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
